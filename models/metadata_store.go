f64ce84f1e2c95a467fb4768c541d95133886802
Andrew Opem <andrewopem@gmail.com>
2023-01-02 21:52:59 -0800

    consolidate metadata model

--- models/metadata_store.go
+++ models/metadata_store.go
@@ -1,5 +1,21 @@
 package models
 
+type Maintainer struct {
+	Name  string `yaml:"name" binding:"required"`
+	Email string `yaml:"email" binding:"required,email"`
+}
+
+type Metadata struct {
+	Title       string       `yaml:"title" binding:"required"`
+	Version     string       `yaml:"version" binding:"required"`
+	Maintainers []Maintainer `yaml:"maintainers" binding:"required,dive"`
+	Company     string       `yaml:"company" binding:"required"`
+	Website     string       `yaml:"website" binding:"required,url"`
+	Source      string       `yaml:"source" binding:"required"`
+	License     string       `yaml:"license" binding:"required"`
+	Description string       `yaml:"description" binding:"required"`
+}
+
 type MetadataStore struct {
 	Id       string    `yaml:"id" binding:"omitempty,required,alphanum,len=8"`
 	Metadata *Metadata `yaml:"metadata" binding:"required,dive"`
